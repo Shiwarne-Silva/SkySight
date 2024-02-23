c92e47288c167e2473ba25c2d78ab377beb7c570
Shiwarne-MBiz <137801291+Shiwarne-MBiz@users.noreply.github.com>
2024-02-24 03:17:23 +0530

    conflicts resolved app completed

--- app/src/main/java/com/example/skysight/MainActivity.kt
+++ app/src/main/java/com/example/skysight/MainActivity.kt
@@ -1,43 +1,47 @@
 package com.example.skysight
 
 import android.os.AsyncTask
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ProgressBar
 import android.widget.RelativeLayout
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.skysight.R
 import org.json.JSONObject
 import java.net.URL
 import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    val CITY : String = "Kandana, SL"
-    val API : String = "6578a3ed83079c8a55e90996f38ebe3a"
+
+    val CITY: String = "Bradford"
+    val API: String = "6578a3ed83079c8a55e90996f38ebe3a"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         weatherTask().execute()
-    }
-    inner class weatherTask() : AsyncTask<String, Unit, String>()
 
-    {
-        override fun onPreExecute(){
+    }
+
+    inner class weatherTask() : AsyncTask<String, Void, String>() {
+        override fun onPreExecute() {
             super.onPreExecute()
+            /* Showing the ProgressBar, Making the main design GONE */
             findViewById<ProgressBar>(R.id.loader).visibility = View.VISIBLE
             findViewById<RelativeLayout>(R.id.mainContainer).visibility = View.GONE
-            findViewById<TextView>(R.id.errortext).visibility = View.GONE
+            findViewById<TextView>(R.id.errorText).visibility = View.GONE
         }
 
-        override fun doInBackground(vararg p0: String?): String? {
+        override fun doInBackground(vararg params: String?): String? {
             var response:String?
-            try {
-                response = URL("https://api.openweathermap.org/data/2.5/weather?q=$CITY&units=metric&appid=$API")
-                    .readText(Charsets.UTF_8)
-            }catch (e:Exception){
+            try{
+                response = URL("https://api.openweathermap.org/data/2.5/weather?q=$CITY&units=metric&appid=$API").readText(
+                    Charsets.UTF_8
+                )
+            }catch (e: Exception){
                 response = null
             }
             return response
@@ -45,29 +49,32 @@ class MainActivity : AppCompatActivity() {
 
         override fun onPostExecute(result: String?) {
             super.onPostExecute(result)
-            try{
+            try {
+                /* Extracting JSON returns from the API */
                 val jsonObj = JSONObject(result)
                 val main = jsonObj.getJSONObject("main")
                 val sys = jsonObj.getJSONObject("sys")
                 val wind = jsonObj.getJSONObject("wind")
                 val weather = jsonObj.getJSONArray("weather").getJSONObject(0)
+
                 val updatedAt:Long = jsonObj.getLong("dt")
-                val updatedAtText = "Updated at: "+SimpleDateFormat("dd/MM/yyyy hh:mm", Locale.ENGLISH).format(
-                    Date(updatedAt*1000)
-                )
+                val updatedAtText = "Updated at: "+ SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(Date(updatedAt*1000))
                 val temp = main.getString("temp")+"°C"
-                val tempMin = "Min Temp: "+main.getString("temp_min")+"°C"
-                val tempMax = "Max Temp: "+main.getString("temp_max")+"°C"
+                val tempMin = "Min Temp: " + main.getString("temp_min")+"°C"
+                val tempMax = "Max Temp: " + main.getString("temp_max")+"°C"
                 val pressure = main.getString("pressure")
                 val humidity = main.getString("humidity")
+
                 val sunrise:Long = sys.getLong("sunrise")
                 val sunset:Long = sys.getLong("sunset")
                 val windSpeed = wind.getString("speed")
                 val weatherDescription = weather.getString("description")
+
                 val address = jsonObj.getString("name")+", "+sys.getString("country")
 
+                /* Populating extracted data into our views */
                 findViewById<TextView>(R.id.address).text = address
-                findViewById<TextView>(R.id.updated_at).text = updatedAtText
+                findViewById<TextView>(R.id.updated_at).text =  updatedAtText
                 findViewById<TextView>(R.id.status).text = weatherDescription.capitalize()
                 findViewById<TextView>(R.id.temp).text = temp
                 findViewById<TextView>(R.id.temp_min).text = tempMin
@@ -78,13 +85,15 @@ class MainActivity : AppCompatActivity() {
                 findViewById<TextView>(R.id.pressure).text = pressure
                 findViewById<TextView>(R.id.humidity).text = humidity
 
+                /* Views populated, Hiding the loader, Showing the main design */
                 findViewById<ProgressBar>(R.id.loader).visibility = View.GONE
                 findViewById<RelativeLayout>(R.id.mainContainer).visibility = View.VISIBLE
-            }
-            catch (e: Exception){
+
+            } catch (e: Exception) {
                 findViewById<ProgressBar>(R.id.loader).visibility = View.GONE
-                findViewById<TextView>(R.id.errortext).visibility = View.VISIBLE
+                findViewById<TextView>(R.id.errorText).visibility = View.VISIBLE
             }
+
         }
     }
 }
